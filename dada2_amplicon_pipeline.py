6d0db04492a4b97ce277686d296a1104623605bb
Tommyvanwijk <tom_van_wijk@hotmail.com>
2018-06-21 17:59:40 +0200

    first version

---
+++ dada2_amplicon_pipeline.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+
+# Name:		dada2_amplicon_pipeline.py
+# Author:	Tom van Wijk - RIVM Bilthoven
+# Date:		21-06-2018
+# Licence:	GNU General Public License v3.0 (copy provided in directory)
+
+# For detailed information and instruction on how to install and use this software
+# please view the included "README.md" file in this repository
+
+
+# import python libraries
+from argparse import ArgumentParser
+from time import gmtime, strftime
+import os
+import sys
+import logging
+import logging.handlers
+import random
+
+
+# Function to parse the command-line arguments
+# Returns a namespace with argument keys and values
+def parse_arguments(args, log):
+	log.info("Parsing command line arguments...")
+	parser = ArgumentParser(prog="dada2_amplicon_pipeline.py")
+	parser.add_argument("-i", "--indir", dest = "input_dir",
+		action = "store", default = None, type = str,
+		help = "Location of input directory (required)",
+		required = True)
+	parser.add_argument("-o", "--outdir", dest = "output_dir",
+		action = "store", default = "inputdir", type = str,
+		help = "Location of output directory (default=inputdir)")	
+	parser.add_argument("-x", "--savetemp", dest = "save_temp",
+		action = "store", default = "false", type = str,
+		help = "Option to save temporary files (default=false)")
+	return parser.parse_args()
+
+
+# Function creates logger with handlers for both logfile and console output
+# Returns logger
+def create_logger(logid):
+	# create logger
+	log = logging.getLogger()
+	log.setLevel(logging.INFO)
+	# create file handler
+	fh = logging.FileHandler(str(logid)+'_dada2_amplicon_pipeline.log')
+	fh.setLevel(logging.DEBUG)
+	fh.setFormatter(logging.Formatter('%(message)s'))
+	log.addHandler(fh)
+	# create console handler
+	ch = logging.StreamHandler()
+	ch.setLevel(logging.INFO)
+	ch.setFormatter(logging.Formatter('%(message)s'))
+	log.addHandler(ch)
+	return log
+	
+
+# Function creates a list of files or directories in <inputdir>
+# on the specified directory depth
+def list_directory(input_dir, obj_type, depth):
+	dir_depth = 1
+	for root, dirs, files in os.walk(input_dir):
+		if dir_depth == depth:
+			if obj_type ==  'files':
+				return files
+			elif obj_type == 'dirs':
+				return dirs
+		dir_depth += 1
+		
+
+# Function to run the dada2classification script
+# input: directory with processed and filtered/sorted .fastq filepairs
+def run_dada2_classify(input_directory, reference_directory):
+	os.system("dada2_classify_contigs.R "+input_directory+" "+reference_directory
+		+" | tee "+input_directory+"/dada2_classify_contigs.log")
+
+
+# Function closes logger handlers
+def close_logger(log):
+	for handler in log.handlers:
+		handler.close()
+		log.removeFilter(handler)
+
+
+# MAIN function
+def main():
+	# create logger
+	logid = random.randint(99999, 9999999)
+	log = create_logger(logid)
+	# parse command line arguments
+	args = parse_arguments(sys.argv, log)
+	# creating output directory
+	if args.output_dir == 'inputdir':
+		output_dir = os.path.abspath(args.input_dir+"/dada2_amplicon_pipeline_output")
+	else:
+		output_dir = os.path.abspath(args.output_dir)
+	log.info("Creating output directory: "+output_dir)
+	os.system("mkdir -p "+output_dir)
+	# create directories for logfiles, temporary data and output in output_dir
+	os.system("mkdir -p "+output_dir+"/temp")
+	os.system("mkdir -p "+output_dir+"/logfiles")
+	os.system("mkdir -p "+output_dir+"/quality_control")
+	# copy data to temp dir
+	os.system("cp "+args.input_dir+"/*.fastq "+output_dir+"/temp/.")
+	os.system("cp "+args.input_dir+"/*.fastq.gz "+output_dir+"/temp/.")
+	# run dada2 script
+	log.info("Executing the dada2 classification script")
+	run_dada2_classify(os.path.abspath(output_dir+"/temp"), os.environ['DADA2_AMPLICON_HOME'])
+	# Move relevant dada2 output to output and output/logfiles directory
+	os.system("mv "+output_dir+"/temp/*.log "+output_dir+"/logfiles/")
+	os.system("mv "+output_dir+"/temp/*.pdf "+output_dir+"/quality_control/")
+	os.system("mv "+output_dir+"/temp/*.rds "+output_dir+"/")
+	os.system("mv "+output_dir+"/temp/*.txt "+output_dir+"/")
+	# Remove the temp directory if temp parameter is not set to anything but false
+	if args.save_temp == "false":
+                log.info("save_temp parameter is false (default). Removing temporary files and directories...")
+		os.system("rm -rf "+output_dir+"/temp")
+	else:
+		log.info("save_temp parameter is not false (set in input parameter). Keeping temporary files and directories...")
+	# close logger handlers
+	log.info("Closing logger and finalising dada2_amplicon_pipeline.py...")
+	close_logger(log)
+	# move logfile to output directory
+	os.system("mv "+str(logid)+"_dada2_amplicon_pipeline.log "+output_dir+"/logfiles/dada2_amplicon_pipeline.log")
+
+
+main()
---
+++ dada2_classify_contigs.R
@@ -0,0 +1,139 @@
+#!/usr/bin/env Rscript
+
+
+# Name:		dada2_classify_contigs.R
+# Author:	Tom van Wijk - RIVM Bilthoven
+# Date:		12-12-2017
+# Licence:	GNU General Public License v3.0 (copy provided in directory)
+
+# This R script is a subscript of dada2_amplicon_pipeline.py
+# For detailed information and instruction on how to install and use this software
+# please view the included "README.md" file in this repository.
+
+# The script can also be run separate from dada2_amplicon_pipeline.py
+# with the following command, however, this is not recommended.
+
+# Run with command: 'dada2_classify_contigs.R <input_dir> <reference_dir>
+# input_dir:		FULL path! to input directory with .fastq files
+# reference_dir:	FULL path! to durectory with reference files
+
+# Requirements:
+# - R v3.4.2 of later
+# - Bioconductor v3.6 or later
+# - dada 2 (R package)
+
+
+# import R packages
+library(dada2); packageVersion("dada2")
+
+# Parse command line args
+args = commandArgs(trailingOnly=TRUE)
+
+# check if parameters are given
+if (length(args) != 2) stop ("ERROR: Exactly 2 parameters must be given.")
+setwd(args[1])
+refpath = args[2]
+list.files()
+
+# get separate lists of forward and reverse reads
+target_R1 <- c("_R1", ".fastq")
+target_R2 <- c("_R2", ".fastq")
+FR_files <- sort(list.files(pattern=target_R1, full.names = TRUE))
+RR_files <- sort(list.files(pattern=target_R2, full.names = TRUE))
+
+# check if no. of forward and reverse files are the same
+if (length(FR_files) != length(RR_files)) stop ("ERROR: Number of forward and reverse files do not match.")
+
+# check if forward and reverse files are properly paired by name
+sample.FR_filenames <- sapply(strsplit(basename(FR_files), "_R1"), `[`, 1)
+sample.RR_filenames <- sapply(strsplit(basename(RR_files), "_R2"), `[`, 1)
+if (!identical(sample.FR_filenames, sample.RR_filenames)) stop ("ERROR: Forward and reverse files do not match.")
+
+# adding names to forward and reverse files
+names(FR_files) <- sample.FR_filenames
+names(RR_files) <- sample.RR_filenames
+
+# create quality rapport of raw input data
+pdf(file = "fastq_quality_profiles_raw.pdf")
+for (sample in sample.FR_filenames) {
+       cat ("Generating quality profile:\t", sample, "\n")
+       print(plotQualityProfile(list(FR_files[sample], RR_files[sample])))
+}
+dev.off()
+
+# Create location for trimmed reads
+FR_files_filt <- file.path(paste0("filtered/", sample.FR_filenames, "_R1_filt.fastq.gz"))
+RR_files_filt <- file.path(paste0("filtered/", sample.RR_filenames, "_R2_filt.fastq.gz"))
+
+# adding names to location of filtered forward and reverse files
+names(FR_files_filt) <- sample.FR_filenames
+names(RR_files_filt) <- sample.RR_filenames
+
+# filtering and trimming the reads from FR_files to FR_files_filt
+# WARNING: THESE PARAMETERS AREN'T OPTIMAL FOR ALL DATASETS, please inform "README.MD"
+filterAndTrim(fwd=FR_files, filt=FR_files_filt,	rev=RR_files, filt.rev=RR_files_filt,
+			  truncLen=c(180,160), maxEE=2, truncQ=11, maxN=0, rm.phix=TRUE,
+			  compress=TRUE, verbose=TRUE, multithread=TRUE)
+
+# create quality rapport of filtered and trimmed input data
+pdf(file = "fastq_quality_profiles_filtered.pdf")
+for (sample in sample.FR_filenames) {
+       cat ("Generating quality profile:\t", sample, "\n")
+       print(plotQualityProfile(list(FR_files_filt[sample], RR_files_filt[sample])))
+}
+dev.off()
+	
+set.seed(100)
+
+# learn error rates of files
+FR_err <- learnErrors(FR_files_filt, nread=1e6, multithread=TRUE)
+RR_err <- learnErrors(RR_files_filt, nread=1e6, multithread=TRUE)
+
+# create error plots of forward and reverse fastq files
+pdf(file = "error_plot_FR.pdf")
+plotErrors(FR_err, nominalQ=TRUE)
+dev.off()
+pdf(file = "error_plot_RR.pdf")
+plotErrors(RR_err, nominalQ=TRUE)
+dev.off()
+
+# Sample inference and merger of paired-end reads
+mergers <- vector("list", length(sample.FR_filenames))
+names(mergers) <- sample.FR_filenames
+
+# Merge samples
+for (sample in sample.FR_filenames) {
+	cat ("Processing:\t", sample, "\n")
+	F_derep <- derepFastq(FR_files_filt[[sample]])
+	F_dd <- dada(F_derep, err=FR_err, multithread=TRUE)
+	R_derep <- derepFastq(RR_files_filt[[sample]])
+	R_dd <- dada(R_derep, err=RR_err, multithread=TRUE)
+	merger <- mergePairs(F_dd, F_derep, R_dd, R_derep)
+	mergers[[sample]] <- merger
+}
+
+rm(F_derep)
+rm(R_derep)
+
+# Construct raw sequence table
+seqtab <- makeSequenceTable(mergers)
+
+# Inspect distribution of sequence lengths
+table(nchar(getSequences(seqtab)))
+
+# Remove Chimeras
+seqtab_nochim <- removeBimeraDenovo(seqtab, method="consensus", multithread=TRUE)
+saveRDS(seqtab_nochim, "sequence_table.rds")
+swapped_seqtab <- data.frame(t(seqtab_nochim))
+write.table(swapped_seqtab, "sequence_table.txt", sep="\t", col.names=NA)
+
+# Assign taxonomy
+tax6 <- assignTaxonomy(seqtab_nochim, paste(refpath, "/silva_nr_v128_train_set.fa.gz", sep = ""), multithread=TRUE)
+tax7 <- addSpecies(tax6, paste(refpath, "/silva_species_assignment_v128.fa.gz", sep = ""))
+saveRDS(tax6, "taxonomy_level6_genus.rds")
+saveRDS(tax7, "taxonomy_level7_species.rds")
+write.table(tax6, "taxonomy_level6_genus.txt", sep="\t", col.names=NA)
+write.table(tax7, "taxonomy_level7_species.txt", sep="\t", col.names=NA)
+
+# check if number of entries in tax and seqtab files are the same
+if (nrow(swapped_seqtab) != nrow(tax7)) stop ("ERROR: seqtab and taxonomy file to not match.")
